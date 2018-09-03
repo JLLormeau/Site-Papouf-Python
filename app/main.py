82bdbc617627e765281c303293afd8148d832b04
colette <colette.lormeau@outlook.com>
2018-09-03 17:17:56 +0200

    Fix bad repo

--- app/main.py
+++ app/main.py
@@ -13,7 +13,11 @@ def about():
 def moumout ():
     return render_template('moumout.html')
 
+@app.route('/babykiki/')
+def babykiki ():
+    return render_template('babykiki.html')
+
 if __name__ == '__main__':
-  # Run the app server on localhost:4449
+  # Run the app server on localhost:5000
   # app.run()
    app.run()
